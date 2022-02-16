0b1062af2741eab9f448c51b0272d93a3ef27a92
lijiaze05 <lijiaze05@meituan.com>
2022-02-16 18:13:15 +0800

    add AlexNet

--- Lenet/train.py
+++ Lenet/train.py
@@ -24,7 +24,37 @@ def main():
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=10000, shuffle=False, num_workers=0)
     val_data_iter = iter(val_loader)
     val_image, val_label = val_data_iter.next()
-    print(len(val_label))
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    net = LeNet()
+
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    for epoch in range(10):
+        running_loss = 0.0
+        for step, data in enumerate(train_loader, start=0):
+            inputs, labels = data
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if step % 500 == 499:
+                with torch.no_grad():
+                    outputs = net(val_image)
+                    predict_y = torch.max(outputs, dim=1)[1]
+                    accuracy = torch.eq(predict_y, val_label).sum().item()/val_label.size(0)
+                    print('[%d, %5d] train_loss %.3f test_accuracy: %.3f' %(epoch+1, step+1, running_loss/500, accuracy))
+                    running_loss = 0
+    print("Finished Training")
+
+    save_path = './Lenet.pth'
+    torch.save(net.state_dict(), save_path)
 
 if __name__=='__main__':
     main()
